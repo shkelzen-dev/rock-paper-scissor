89ed9eea6ae21b1d987e17b21d77b8f2169e3c43
Mr.Robot <kenitafili@gmail.com>
2022-11-26 15:39:14 +0100

    user interface

--- game.js
+++ game.js
@@ -18,10 +18,13 @@ function getComputerChoice(){
     else{
         computerChoice = 'scissor' ;
     }
-
+    document.querySelector('.computerChoice').textContent = 'Computer plays: '+computerChoice;
     return computerChoice ; 
 }
 
+
+const result = document.querySelector('.result');
+const points = document.querySelector('.points');
 
 
 
@@ -31,38 +34,56 @@ function playRound(playerSelection, computerSelection){
     
      
     if((playerSelection == 'rock' && computerSelection == 'rock')||(playerSelection == 'paper' && computerSelection == 'paper')||(playerSelection == 'scissor' && computerSelection == 'scissor')){
-        console.log('Equal!');
+        
+        result.textContent = 'Equal!';
     }
     else if(playerSelection == 'rock' && computerSelection == 'paper'){
         computerPoint+=1;
-        console.log('You Lose , because Paper beats Rock!');
+        
+        result.textContent = 'You Lose , because Paper beats Rock! :(';
     }
     else if(playerSelection == 'rock' && computerSelection == 'scissor'){
         playerPoint+=1;
-        console.log('You Win , because Rock beats Scissor!');
+       
+        result.textContent = 'You Win , because Rock beats Scissor! :)';
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock'){
         playerPoint+=1
-        console.log('You Win , because Paper beats Rock!');
+        
+        result.textContent = 'You Win , because Paper beats Rock! :)';
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissor'){
         computerPoint+=1;
-        console.log('You Lose , because Scissor beats Paper!');
+        
+        result.textContent = 'You Lose , because Scissor beats Paper! :(';
     }
     else if (playerSelection == 'scissor' && computerSelection == 'rock'){
         computerPoint+=1;
-        console.log('You Lose , because Rock beats Scissor!');
+        
+        result.textContent = 'You Lose , because Rock beats Scissor! :(';
     }
     else if (playerSelection == 'scissor' && computerSelection == 'paper'){
         playerPoint+=1;
-        console.log( 'You Win , because Scissor beats Paper!');
+        
+        result.textContent = 'You Win , because Scissor beats Paper! :)';
     }
     else{
-        console.log( 'Something is not right with your code! :( ');
+       
+        result.textContent = 'Something is not right with your code! :( ' ; 
     }
     
-    console.log(`Computer is ${computerPoint} point and Player is ${playerPoint} point ! `);
-   
+    points.textContent = `Player is ${playerPoint} point and Computer is ${computerPoint} point !`;
+    if(computerPoint >= 5 ){
+        document.querySelector('.computerChoice').textContent = '';
+        points.textContent = '';
+        result.textContent = 'Computer is winner!';
+    }
+     else if (playerPoint >= 5){
+        document.querySelector('.computerChoice').textContent = '';
+        points.textContent = '';
+        result.textContent = 'Player is winner !';
+    
+    }
 }
 
 //points counter
@@ -70,26 +91,22 @@ let computerPoint = 0 ;
 let playerPoint = 0 ;
 
 
-//play 5 rounds
-function game(){
-    
-
-    for(let i = 0 ;i<5;i++){
-       
-        const computerSelection = getComputerChoice();
-        
-        let playerChoice = prompt('User input:');
-        const playerSelection = playerChoice.toLowerCase();
-        console.log(computerSelection);
-
-        playRound(playerSelection , computerSelection);
-        
-    }
-    
-
-}
-
-game();
 
 
 
+//selecting buttons
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorBtn = document.getElementById('scissor');
+
+//set playerSelection equal to button value
+rockBtn.addEventListener('click' , ()=>{
+    playRound('rock',getComputerChoice());
+});
+
+paperBtn.addEventListener('click', ()=>{
+    playRound('paper',getComputerChoice());
+});
+scissorBtn.addEventListener('click',()=>{
+    playRound('scissor',getComputerChoice());
+});
